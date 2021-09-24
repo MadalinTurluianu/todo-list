7c63d6925ca2d0374215c206db6abd83edae7e05
MadalinTurluianu <turluianu.petre.madalin@gmail.com>
2021-09-24 14:59:22 +0300

    weekly filter done

--- src/components/filteredLists.js
+++ src/components/filteredLists.js
@@ -29,6 +29,7 @@ function createFilteredList(filterType, originList, projectName = "") {
   } else if (filterType === "week") {
     filteredArray = originList.filter((task) => {
       const week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
       let taskDateArr = new Date(task.taskObj.dueDate)
         .toString()
         .split(" ")
@@ -36,23 +37,94 @@ function createFilteredList(filterType, originList, projectName = "") {
       let taskMonth = taskDateArr[1];
       let taskDay = taskDateArr[2];
       let taskYear = taskDateArr[3];
-      let index = week.indexOf(currentDate.split(" ")[0]);
+      let currentDay = Number(currentDate.split(" ")[2]);
+      let currentMonth = currentDate.split(" ")[1];
+      let currentYear = currentDate.split(" ")[3];
+      let weekIndex = week.indexOf(currentDate.split(" ")[0]);
 
-      console.log(currentDate.split(" ")[2]);
-      console.log(taskDay);
+      const months = {
+        all: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        31: ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"],
+        30: ["Apr", "Jun", "Sep", "Nov"],
+        29: Number(currentYear) % 4 === 0 ? ["Feb"] : [],
+        28: Number(currentYear) % 4 === 0 ? [] : ["Feb"],
+      };
 
-      for (let i = 0; i <= index; i++) {
-        if (
-          `${taskDay} ${taskMonth} ${taskYear}` ===
-          `${Number(currentDate.split(" ")[2]) - index - i} ${currentDate.split(" ")[1]} ${currentDate.split(" ")[3]}`
-        ) {
-          return true;
+      for (let i = weekIndex; i <= weekIndex + 6; i++) {
+        let thisWeekDay = currentDay + (6 - i);
+        let thisWeekMonth = currentMonth;
+        let thisWeekYear = currentYear;
+
+        // ---GET TO NEXT MONTH/YEAR
+
+        function setNextMonthYear() {
+          if (thisWeekMonth === "Dec") {
+            thisWeekMonth = "Jan";
+            thisWeekYear = (Number(thisWeekYear) + 1).toString();
+          } else {
+            thisWeekMonth = months.all[months.all.indexOf(thisWeekMonth) + 1];
+          }
         }
-      }
-      for (let i = index+1; i < 6; i++) {
+
+        function setPreviousMonthYear() {
+          if (thisWeekMonth === "Jan") {
+            thisWeekMonth = "Dec";
+            thisWeekYear = (Number(thisWeekYear) - 1).toString();
+          } else {
+            thisWeekMonth = months.all[months.all.indexOf(thisWeekMonth) - 1];
+          }
+        }
+
+        if (months[31].includes(thisWeekMonth) && thisWeekDay > 31) {
+          thisWeekDay = thisWeekDay - 31;
+          setNextMonthYear();
+        } else if (months[30].includes(thisWeekMonth) && thisWeekDay > 30) {
+          setNextMonthYear();
+          thisWeekDay = thisWeekDay - 30;
+        } else if (months[29].includes(thisWeekMonth) && thisWeekDay > 29) {
+          thisWeekDay = thisWeekDay - 29;
+          setNextMonthYear();
+        } else if (months[28].includes(thisWeekMonth) && thisWeekDay > 28) {
+          thisWeekDay = thisWeekDay - 28;
+          setNextMonthYear();
+        }
+
+        // ---GET TO PREVIOUS MONTH/YEAR
+        else if (thisWeekDay < 1) {
+          setPreviousMonthYear();
+
+          if (months[31].includes(thisWeekMonth)) {
+            thisWeekDay = 31 + thisWeekDay;
+          } else if (months[30].includes(thisWeekMonth)) {
+            thisWeekDay = 30 + thisWeekDay;
+          } else if (months[29].includes(thisWeekMonth)) {
+            thisWeekDay = 29 + thisWeekDay;
+          } else if (months[28].includes(thisWeekMonth)) {
+            thisWeekDay = 28 + thisWeekDay;
+          }
+        }
+
+        // ---NORMAL BEHAVIOR
+        else {
+          thisWeekDay = thisWeekDay;
+        }
+
         if (
-          `${taskDay} ${taskMonth} ${taskYear}` ===
-          `${Number(currentDate.split(" ")[2]) + 6 - i} ${currentDate.split(" ")[1]} ${currentDate.split(" ")[3]}`
+          `${Number(taskDay)} ${taskMonth} ${taskYear}` ===
+          `${thisWeekDay} ${thisWeekMonth} ${thisWeekYear}`
         ) {
           return true;
         }
